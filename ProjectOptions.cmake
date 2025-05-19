c2a39df82d5c0b5ee782e223417b88fd3266d035
Template Janitor <template.janitor@example.com>
2025-05-19 07:29:14 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -4,7 +4,7 @@ include(CMakeDependentOption)
 include(CheckCXXCompilerFlag)
 
 
-macro(myproject_supports_sanitizers)
+macro(BphoOptics_supports_sanitizers)
   if((CMAKE_CXX_COMPILER_ID MATCHES ".*Clang.*" OR CMAKE_CXX_COMPILER_ID MATCHES ".*GNU.*") AND NOT WIN32)
     set(SUPPORTS_UBSAN ON)
   else()
@@ -18,183 +18,183 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(BphoOptics_setup_options)
+  option(BphoOptics_ENABLE_HARDENING "Enable hardening" ON)
+  option(BphoOptics_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    BphoOptics_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    BphoOptics_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  BphoOptics_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR BphoOptics_PACKAGING_MAINTAINER_MODE)
+    option(BphoOptics_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(BphoOptics_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(BphoOptics_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(BphoOptics_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(BphoOptics_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(BphoOptics_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(BphoOptics_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(BphoOptics_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(BphoOptics_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(BphoOptics_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(BphoOptics_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(BphoOptics_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(BphoOptics_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(BphoOptics_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(BphoOptics_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(BphoOptics_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(BphoOptics_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(BphoOptics_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(BphoOptics_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(BphoOptics_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(BphoOptics_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(BphoOptics_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(BphoOptics_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(BphoOptics_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(BphoOptics_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(BphoOptics_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_USER_LINKER
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      BphoOptics_ENABLE_IPO
+      BphoOptics_WARNINGS_AS_ERRORS
+      BphoOptics_ENABLE_USER_LINKER
+      BphoOptics_ENABLE_SANITIZER_ADDRESS
+      BphoOptics_ENABLE_SANITIZER_LEAK
+      BphoOptics_ENABLE_SANITIZER_UNDEFINED
+      BphoOptics_ENABLE_SANITIZER_THREAD
+      BphoOptics_ENABLE_SANITIZER_MEMORY
+      BphoOptics_ENABLE_UNITY_BUILD
+      BphoOptics_ENABLE_CLANG_TIDY
+      BphoOptics_ENABLE_CPPCHECK
+      BphoOptics_ENABLE_COVERAGE
+      BphoOptics_ENABLE_PCH
+      BphoOptics_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  BphoOptics_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (BphoOptics_ENABLE_SANITIZER_ADDRESS OR BphoOptics_ENABLE_SANITIZER_THREAD OR BphoOptics_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(BphoOptics_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(BphoOptics_global_options)
+  if(BphoOptics_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    BphoOptics_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  BphoOptics_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(BphoOptics_ENABLE_HARDENING AND BphoOptics_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR BphoOptics_ENABLE_SANITIZER_UNDEFINED
+       OR BphoOptics_ENABLE_SANITIZER_ADDRESS
+       OR BphoOptics_ENABLE_SANITIZER_THREAD
+       OR BphoOptics_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${BphoOptics_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${BphoOptics_ENABLE_SANITIZER_UNDEFINED}")
+    BphoOptics_enable_hardening(BphoOptics_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(BphoOptics_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(BphoOptics_warnings INTERFACE)
+  add_library(BphoOptics_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  BphoOptics_set_project_warnings(
+    BphoOptics_warnings
+    ${BphoOptics_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
     "")
 
-  if(myproject_ENABLE_USER_LINKER)
+  if(BphoOptics_ENABLE_USER_LINKER)
     include(cmake/Linker.cmake)
-    myproject_configure_linker(myproject_options)
+    BphoOptics_configure_linker(BphoOptics_options)
   endif()
 
   include(cmake/Sanitizers.cmake)
-  myproject_enable_sanitizers(
-    myproject_options
-    ${myproject_ENABLE_SANITIZER_ADDRESS}
-    ${myproject_ENABLE_SANITIZER_LEAK}
-    ${myproject_ENABLE_SANITIZER_UNDEFINED}
-    ${myproject_ENABLE_SANITIZER_THREAD}
-    ${myproject_ENABLE_SANITIZER_MEMORY})
+  BphoOptics_enable_sanitizers(
+    BphoOptics_options
+    ${BphoOptics_ENABLE_SANITIZER_ADDRESS}
+    ${BphoOptics_ENABLE_SANITIZER_LEAK}
+    ${BphoOptics_ENABLE_SANITIZER_UNDEFINED}
+    ${BphoOptics_ENABLE_SANITIZER_THREAD}
+    ${BphoOptics_ENABLE_SANITIZER_MEMORY})
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(BphoOptics_options PROPERTIES UNITY_BUILD ${BphoOptics_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(BphoOptics_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      BphoOptics_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(BphoOptics_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    BphoOptics_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(BphoOptics_ENABLE_CLANG_TIDY)
+    BphoOptics_enable_clang_tidy(BphoOptics_options ${BphoOptics_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(BphoOptics_ENABLE_CPPCHECK)
+    BphoOptics_enable_cppcheck(${BphoOptics_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(BphoOptics_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    BphoOptics_enable_coverage(BphoOptics_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(BphoOptics_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(BphoOptics_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(BphoOptics_ENABLE_HARDENING AND NOT BphoOptics_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR BphoOptics_ENABLE_SANITIZER_UNDEFINED
+       OR BphoOptics_ENABLE_SANITIZER_ADDRESS
+       OR BphoOptics_ENABLE_SANITIZER_THREAD
+       OR BphoOptics_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    BphoOptics_enable_hardening(BphoOptics_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
--- src/ftxui_sample/main.cpp
+++ src/ftxui_sample/main.cpp
@@ -25,7 +25,7 @@
 #include <lefticus/tools/non_promoting_ints.hpp>
 
 // This file will be generated automatically when cur_you run the CMake
-// configuration step. It creates a namespace called `myproject`. You can modify
+// configuration step. It creates a namespace called `BphoOptics`. You can modify
 // the source template at `configured_files/config.hpp.in`.
 #include <internal_use_only/config.hpp>
 
@@ -332,7 +332,7 @@ void game_iteration_canvas()
 int main(int argc, const char **argv)
 {
   try {
-    CLI::App app{ fmt::format("{} version {}", myproject::cmake::project_name, myproject::cmake::project_version) };
+    CLI::App app{ fmt::format("{} version {}", BphoOptics::cmake::project_name, BphoOptics::cmake::project_version) };
 
     std::optional<std::string> message;
     app.add_option("-m,--message", message, "A message to print back out");
@@ -352,7 +352,7 @@ int main(int argc, const char **argv)
     CLI11_PARSE(app, argc, argv);
 
     if (show_version) {
-      fmt::print("{}\n", myproject::cmake::project_version);
+      fmt::print("{}\n", BphoOptics::cmake::project_version);
       return EXIT_SUCCESS;
     }
 
